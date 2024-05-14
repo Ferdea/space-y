2472ba379ae3feeab4823ff7c157c9be6b36c243
LegendaV <115637903+LegendaV@users.noreply.github.com>
2024-05-14 18:06:47 +0500

    3

--- server.mjs
+++ server.mjs
@@ -24,6 +24,14 @@ app.get("/", (_, res) => {
   res.send(":)");
 });
 
-app.listen(port, () => {
+https
+    .createServer(
+        {
+          key: fs.readFileSync('certs/server.key'),
+          cert: fs.readFileSync('certs/server.cert')
+        },
+        app
+    )
+    .listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
